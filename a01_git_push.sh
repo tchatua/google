c4a0cb699706bd353a79b0efc1ee084b90fad3a7
GOUMGUE SEVAN <gmgsevan@gmail.com>
2026-05-24 17:35:43 -0400

    Managed Group Instance terraform code updated in my GCP Platform infrastructure

---
+++ a01_git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git status
+
+git add .
+
+git status
+
+git commit -m "Managed Group Instance terraform code updated in my GCP Platform infrastructure"
+
+git status
+
+git push --set-upstream origin develop
